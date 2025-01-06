f3c028dfa7ad7770564b106d96e066a9c3687635
amishabaraskar <amishabaraskar.27@gmail.com>
2025-01-06 19:52:17 +0530

    updated playlist fetch command

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -19,7 +19,7 @@ const Page =async  ({params}:{params:Promise<{id:string}>}) => {
     const [post, { select: editorPosts }] = await Promise.all([
         client.fetch(STARTUPS_BY_ID_QUERY, { id }),
         client.fetch(PLAYLIST_BY_SLUG_QUERY, {
-            slug: "editor-picks-new",
+            slug: "editor-picks",
         }),
     ]);
     // const post=await client.fetch(STARTUPS_BY_ID_QUERY,{id})
